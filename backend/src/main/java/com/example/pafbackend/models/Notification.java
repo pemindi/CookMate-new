ef07f9c0241b5f1e46484c06e4c4cdb8d11cd314
Pemindi Wimalarathna <159780628+pemindi@users.noreply.github.com>
2025-05-05 15:05:36 +0530

    User Management & Auth stuff

--- backend/src/main/java/com/example/pafbackend/models/Notification.java
+++ backend/src/main/java/com/example/pafbackend/models/Notification.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import java.util.Date;
+//import java.util.Date;
 
 @Document(collection = "notifications")
 @Getter
